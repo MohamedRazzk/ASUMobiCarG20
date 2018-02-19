69ff9ef35f56c3f83cfa7fd0f9519188dbe350c8
Mohamed F. Razzk <mohamedrazzk@gmail.com>
2018-02-19 14:54:19 +0200

    Individual Prototypes

---
+++ Individual Prototype/Mohamed Razzk/Blink/Blink.ino
@@ -0,0 +1,33 @@
+/*
+**********************************************************************
+* Project           : AsuMobiCar
+*
+* Program name      : Blink
+* 
+* File              : Blink.ino
+*
+* Author            : Mohamed Fathi Mohamed Razzk 
+*
+* Date created      : 19/2/2018 - 2 PM 
+*
+* Purpose           : Delayig a led 
+* 
+* Cource            : CSE271  - System Dynamics & Control Components
+*
+* Revision History  : ---------------
+*
+**********************************************************************
+*/
+
+void setup() {
+  // initialize digital pin 13 as an output.
+  pinMode(13, OUTPUT);
+}
+
+// the loop function runs over and over again forever
+void loop() {
+  digitalWrite(13, HIGH);   // turn the LED on (HIGH is the voltage level)
+  delay(1000);              // wait for a second
+  digitalWrite(13, LOW);    // turn the LED off by making the voltage LOW
+  delay(1000);              // wait for a second
+}
---
+++ Individual Prototype/Mohamed Razzk/Serial Read/Serialread.ino
@@ -0,0 +1,45 @@
+/*
+**********************************************************************
+* Project           : AsuMobiCar
+*
+* Program name      : Serial Read 
+* 
+* File              : Serialread.ino
+*
+* Author            : Mohamed Fathi Mohamed Razzk 
+*
+* Date created      : 19/2/2018 - 2:30 PM 
+*
+* Purpose           : Reading input from computer 
+* 
+* 
+* Cource            : CSE271  - System Dynamics & Control Components
+*
+* Revision History  : ---------------
+*
+**********************************************************************
+*/
+// include the library code:
+#include <LiquidCrystal.h>
+
+// initialize the library with the numbers of the interface pins
+LiquidCrystal lcd(12, 11, 5, 4, 3, 2);
+
+void setup() {
+  // set up the LCD's number of columns and rows:
+  lcd.begin(16, 2);
+  // Print a message to the LCD.
+  lcd.print("Serial Read Screen");
+  Serial.begin(9600);
+}
+
+void loop() {
+  // set the cursor to column 0, line 1
+  // (note: line 1 is the second row, since counting begins with 0):
+  lcd.setCursor(0, 1);
+  // print the number of seconds since reset:
+  lcd.print(millis() / 1000);
+    lcd.print(Serial.read());
+
+}
+
