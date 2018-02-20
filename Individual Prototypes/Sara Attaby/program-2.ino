360f8bdf1bb78d47775bb20aa10176350340bcb0
SaraAttaby <36515304+SaraAttaby@users.noreply.github.com>
2018-02-20 19:50:39 +0200

    Add files via upload

---
+++ Individual Prototypes/Sara Attaby/program-2.ino
@@ -0,0 +1,38 @@
+int LED4=4;
+int LED5=5;
+int LED6=6;
+int LED7=7;
+void setup() {
+  // put your setup code here, to run once:
+pinMode(LED4,OUTPUT);
+pinMode(LED5,OUTPUT);
+pinMode(LED6,OUTPUT);
+pinMode(LED7,OUTPUT);
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+digitalWrite(LED4,HIGH);
+delay(1000);
+digitalWrite(LED4,LOW);
+digitalWrite(LED5,HIGH);
+delay(1000);
+digitalWrite(LED5,LOW);
+digitalWrite(LED6,HIGH);
+delay(1000);
+digitalWrite(LED6,LOW);
+digitalWrite(LED7,HIGH);
+delay(1000);
+digitalWrite(LED7,HIGH);
+delay(1000);
+digitalWrite(LED7,LOW);
+digitalWrite(LED6,HIGH);
+delay(1000);
+digitalWrite(LED6,LOW);
+digitalWrite(LED5,HIGH);
+delay(1000);
+digitalWrite(LED5,LOW);
+digitalWrite(LED4,HIGH);
+delay(1000);
+digitalWrite(LED4,LOW);
+}
