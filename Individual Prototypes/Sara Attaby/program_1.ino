73e3907363ed0885c16610247b96ff45dea65ec0
SaraAttaby <36515304+SaraAttaby@users.noreply.github.com>
2018-02-20 19:49:57 +0200

    Add files via upload

---
+++ Individual Prototypes/Sara Attaby/program_1.ino
@@ -0,0 +1,15 @@
+
+int pulse=12;
+void setup() {
+  // put your setup code here, to run once:
+
+
+pinMode(pulse,OUTPUT);
+}
+void loop() {
+  // put your main code here, to run repeatedly:
+digitalWrite(pulse,HIGH);
+delay(500);
+digitalWrite(pulse,LOW);
+delay(1500);
+}
