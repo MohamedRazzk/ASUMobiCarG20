562e7ab071b41fb96b77934a513adac53dbde476
Mohamed F. Razzk <mohamedrazzk@gmail.com>
2018-03-04 21:25:07 +0200

    Individual

--- Individual Prototypes/Mona Hesham/sketch_feb20a/sketch_feb20a.ino
+++ Individual Prototypes/Mona Hesham/sketch_feb20a/sketch_feb20a.ino
@@ -1,13 +1,24 @@
-int led = 14 ;
+#include <SoftwareSerial.h>
 
-void setup() {
- 
-  pinMode(led, OUTPUT);
+//Change these to whatever pins you're using
+SoftwareSerial blueSerial(0, 1); // RX, TX
+
+void setup()
+{
+  Serial.begin(9600);
+  blueSerial.begin(38400);
+  pinMode(9, OUTPUT);
+  digitalWrite(9, HIGH);
 }
 
-void loop () {
-  digitalWrite(led, HIGH);   
-  delay(100);              
-  digitalWrite(led, LOW);    
-  delay(100);              
+void loop()
+{
+  if (blueSerial.available())
+  {
+    Serial.write(blueSerial.read());
+  }
+  if (Serial.available())
+  {
+    blueSerial.write(Serial.read());
+  }
 }
